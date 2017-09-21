54e9403a9ef80003e134ab9c6316c88ae5aa5fe5
Hubert Jacquin <hj@taktik.com>
2017-09-21 09:52:15 +0200

    Marker manipulation in demo app

--- src/demo/demo-app.ts
+++ src/demo/demo-app.ts
@@ -1,9 +1,17 @@
 import "polymer/polymer.html"
 import './demo-app.html';
 import {customElement} from 'taktik-polymer-typescript'
+import {OzoneVideoPlayer} from "../ozone-video-player"
 
 @customElement('demo-app')
 class demoApp extends Polymer.Element {
+
+    $: {
+        mediaPlayer: OzoneVideoPlayer,
+        addMarker: Element,
+        clear: Element,
+    };
+
     static get properties() {
         return {
             aVideo: {
@@ -27,6 +35,21 @@ class demoApp extends Polymer.Element {
 
     ready() {
         super.ready();
+        this.$.addMarker.addEventListener('click', e => this._addMarker());
+        this.$.clear.addEventListener('click', e => this._clearMarkers());
+    }
+
+    _addMarker(){
+        console.log('_addMarker')
+
+        this.$.mediaPlayer.addMarker({duration:  10, time: 10});
+
+    }
+    _clearMarkers(){
+
+        console.log('_clearMarker')
+        this.$.mediaPlayer.clearMarkers();
+
     }
 
 }
