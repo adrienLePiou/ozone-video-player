256436a42b7fe63442646135eedbcc18654d581b
Hubert Jacquin <hj@taktik.com>
2017-09-21 15:19:17 +0200

    Remove test on browser stack

--- wct.conf.js
+++ wct.conf.js
@@ -41,35 +41,11 @@ var ret = {
         }
     }
 };
-function configBrowserStack(config) {
-    var user = process.env.BROWSERSTACK_USER;
-    var key = process.env.BROWSERSTACK_KEY;
-    if (!user || !key) {
-        throw new Error('Missing BrowserStack credentials. Did you forget to set BROWSERSTACK_USER and/or BROWSERSTACK_KEY?');
-    }
-
-    var url = process.env.BROWSERSTACK_URL
-        || 'http://' + user + ':' + key + '@hub.browserstack.com/wd/hub';
 
 
-    var browsers = ALL_BROWSERS.map(function(b) {
-        b['browserstack.local'] = 'true';
-        b['browserstack.debug'] = 'true';
-        b['url'] = url;
-        return b;
-    });
+console.log('Run test locally');
+ret.plugins.local.browsers = ALL_BROWSERS.map((browser) => browser.browserName);
+ret.plugins.headless.browsers = [ALL_BROWSERS[0]];
 
-    config.activeBrowsers = config.activeBrowsers || [];
-    config.activeBrowsers = config.activeBrowsers.concat(browsers);
-}
-
-if(process.env.BROWSERSTACK_USER) {
-    console.log('Run test on BROWSERSTACK');
-    configBrowserStack(ret);
-} else{
-    console.log('Run test locally');
-    ret.plugins.local.browsers = ALL_BROWSERS.map((browser) => browser.browserName);
-    ret.plugins.headless.browsers = [ALL_BROWSERS[0]];
-}
 
 module.exports = ret;
